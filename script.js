8d5ef472a2badf19149cf0c72d0369fd4b5f9434
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-09-21 02:47:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function displayBrowswerInfo(){
+	let browserName = navigator.appName;
+	ler version = navigator.appVersion;
+
+	const browserinfoDiv = document.getElementById("browser-info");
+
+	const message = "you are using" + browserName +
+	 "version" + version;
+
+	browserinfoDiv.textContent = message;
+	
+}
+window.onload = displayBrowswerInfo;
